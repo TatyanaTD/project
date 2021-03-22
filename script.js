4b00ce04bcbfe3149b95d5fa2f0a528b16069011
Tatyana <halimon_ya@ukr.net>
2021-03-23 00:04:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+
+var leftBorderWidth = 5;
+
+let second = 2;
+const pi = 3.14;
+
+  console.log(4/0);
+  console.log('string'*9);
+
+
+  let something;
+  console.log(something);
+
+  let persone = {
+      name: "John",
+      age: "25",
+      isMarried: false
+  };
+
+  console.log(persone.name);
+
+  console.log(persone["age"]);
+
+  let arr = ['plumb.png', 'orange.jpg', 'apple.bmp'];
+
+  console.log(arr[2]);
+
+  /* alert("Hello Tatyana"); */
+
+  /* let answer = confirm("Are you here?");
+
+  console.log(answer); */
+
+ /*  let answer = prompt("Are you here?", "yes");
+
+  console.log(typeof(answer));
+ */
+
+  console.log("arr" + " - object");
+  console.log(2 + " - object");
+
+  let res = (2 + +"-object");
+
+  console.log(typeof(res));
+
+  let res1 = (2 + "-object");
+
+  console.log(typeof(res1));
+
+  let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log("2" == 2);
+    console.log("2" === 2);
+
+
+/* let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose); */
+
+let isChecked = true,
+    isClose = false;
+
+console.log(! isChecked || isClose);
+
+a = 5;
+console.log(a);
+
+
